28be7db2a8c94efbc79ed6b0e4185a2332901e64
Kyunghwan Kwon <k@mononn.com>
2022-10-06 10:41:47 +0900

    update favicon

--- ports/esp-idf/esp32.cmake
+++ ports/esp-idf/esp32.cmake
@@ -14,6 +14,7 @@ idf_build_process(${BOARD}
 		${BOARD}
 		freertos
 		esptool_py
+		esp-tls
 	SDKCONFIG_DEFAULTS
 		"${CMAKE_CURRENT_LIST_DIR}/sdkconfig.defaults"
 	BUILD_DIR
@@ -36,8 +37,10 @@ add_executable(${PROJECT_EXECUTABLE}
 	${CMAKE_CURRENT_LIST_DIR}/board.c
 	${CMAKE_CURRENT_LIST_DIR}/uart0.c
 	${CMAKE_CURRENT_LIST_DIR}/cli.c
+	${CMAKE_CURRENT_LIST_DIR}/tls.c
 	${APP_SRCS}
 	${CMAKE_SOURCE_DIR}/drivers/wifi/esp32.c
+	${CMAKE_SOURCE_DIR}/ports/coreMQTT/mqtt.c
 )
 
 set(mapfile "${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}.map")
@@ -51,12 +54,22 @@ target_include_directories(${PROJECT_EXECUTABLE}
 		$ENV{IDF_PATH}/components/freertos/include/freertos
 )
 
+include(FetchContent)
+FetchContent_Declare(core_mqtt_src URL https://github.com/FreeRTOS/coreMQTT/archive/refs/tags/v2.0.0.zip)
+FetchContent_Populate(core_mqtt_src)
+include(${core_mqtt_src_SOURCE_DIR}/mqttFilePaths.cmake)
+add_library(core_mqtt ${MQTT_SOURCES} ${MQTT_SERIALIZER_SOURCES})
+target_compile_definitions(core_mqtt PUBLIC MQTT_DO_NOT_USE_CUSTOM_CONFIG)
+target_include_directories(core_mqtt PUBLIC ${MQTT_INCLUDE_PUBLIC_DIRS} ${CMAKE_SOURCE_DIR}/include)
+
 # Link the static libraries to the executable
 target_link_libraries(${PROJECT_EXECUTABLE}
 	idf::${BOARD}
 	idf::freertos
 	idf::spi_flash
 	idf::nvs_flash
+	idf::esp-tls
+	core_mqtt
 	-Wl,--cref
 	-Wl,--Map=\"${mapfile}\"
 )
