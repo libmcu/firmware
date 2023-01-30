cebbd90f6649efbe93793f359bc885ad8dfa48e9
Kyunghwan Kwon <k@mononn.com>
2023-01-30 16:29:06 +0900

    Refactor cmakes

--- projects/app.cmake
+++ projects/app.cmake
@@ -1,7 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
-include(${BASEDIR}/projects/sources.cmake)
-
 add_library(fpl_app OBJECT ${APP_SRCS})
 target_include_directories(fpl_app PUBLIC ${APP_INCS})
 target_compile_definitions(fpl_app PUBLIC ${APP_DEFS})
